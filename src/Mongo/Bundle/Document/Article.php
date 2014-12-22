bb605759ed7dfb178fa12b337ce2fed63c417027
omiranda93 <oscar_miranda93@hotmail.com>
2014-12-22 16:42:13 +0100

    Articulo e incollecions en php, clases creadas

--- src/Mongo/Bundle/Document/Article.php
+++ src/Mongo/Bundle/Document/Article.php
@@ -11,6 +11,97 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
  * @author Juan Antonio
  * @author Oscar
  */
+
+/**
+ * @MongoDB\Document(collection="Articles")
+ */
 class Article {
     //put your code here
+    
+    /**
+     * @MongoDB\Id
+     */
+    protected $id;
+    
+    /**
+     * @MongoDB\Collection
+     */
+    protected $authors = array();
+    
+    /**
+     * @MongoDB\String
+     */
+    protected $title;
+    
+    /**
+     * @MongoDB\Int
+     */
+    protected $year;
+    
+    /**
+     * @MongoDB\String
+     */
+    protected $journal;
+    
+    /**
+     * @MongoDB\String
+     */
+    protected $crossref;
+    
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getAuthors() {
+        return $this->authors;
+    }
+
+    function getTitle() {
+        return $this->title;
+    }
+
+    function getYear() {
+        return $this->year;
+    }
+
+    function getJournal() {
+        return $this->journal;
+    }
+
+    function getCrossref() {
+        return $this->crossref;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    function setAuthors($authors) {
+        $this->authors = $authors;
+        return $this;
+    }
+
+    function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+
+    function setYear($year) {
+        $this->year = $year;
+        return $this;
+    }
+
+    function setJournal($journal) {
+        $this->journal = $journal;
+        return $this;
+    }
+
+    function setCrossref($crossref) {
+        $this->crossref = $crossref;
+        return $this;
+    }
+
+
 }
--- src/Mongo/Bundle/Document/Incollection.php
+++ src/Mongo/Bundle/Document/Incollection.php
@@ -11,6 +11,96 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
  * @author Juan Antonio
  * @author Oscar
  */
+
+/**
+ * @MongoDB\Document(collection="Incollections")
+ */
 class Incollection {
     //put your code here
+    
+    /**
+     * @MongoDB\Id
+     */
+    protected $id;
+    
+    /**
+     * @MongoDB\Collection
+     */
+    protected $author = array();
+    
+    /**
+     * @MongoDB\String
+     */
+    protected $title;
+    
+    /**
+     * @MongoDB\String
+     */
+    protected $bookTitle;
+    
+    /**
+     * @MongoDB\String
+     */
+    protected $crossref;
+    
+    /**
+     * @MongoDB\Int
+     */
+    protected $year;
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getAuthor() {
+        return $this->author;
+    }
+
+    function getTitle() {
+        return $this->title;
+    }
+
+    function getBookTitle() {
+        return $this->bookTitle;
+    }
+
+    function getCrossref() {
+        return $this->crossref;
+    }
+
+    function getYear() {
+        return $this->year;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    function setAuthor($author) {
+        $this->author = $author;
+        return $this;
+    }
+
+    function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+
+    function setBookTitle($bookTitle) {
+        $this->bookTitle = $bookTitle;
+        return $this;
+    }
+
+    function setCrossref($crossref) {
+        $this->crossref = $crossref;
+        return $this;
+    }
+
+    function setYear($year) {
+        $this->year = $year;
+        return $this;
+    }
+
+
 }
