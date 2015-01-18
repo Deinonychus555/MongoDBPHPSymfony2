6286c9d2096f997c5e3535c7bc9fce96b2e26629
omiranda93 <oscar_miranda93@hotmail.com>
2015-01-18 18:29:38 +0100

    Controller nuevo

---
+++ src/Mongo/Bundle/Controller/DataTablesController.php
@@ -0,0 +1,21 @@
+<?php
+namespace Mongo\Bundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DataTablesController
+ *
+ * @author oscarmirandabravo
+ */
+class DataTablesController extends Controller{
+    
+    
+    
+}
